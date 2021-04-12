fd9b44b05847d6eb5c52b472844c91288fb9c51e
ndkv9 <vu.nguyen101@gmail.com>
2021-04-12 21:09:08 +0300

    add ping-endpoint to index.ts

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/ping', (_req, res) => {
+	console.log('there is a ping...');
+	res.send('pong');
+});
+
+app.listen(PORT, () => {
+	console.log(`server running on port ${PORT}`);
+});
