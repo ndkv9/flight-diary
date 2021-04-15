d5fe9e987ad5a1c0cd097a28bbf9c19434b7db29
ndkv9 <vu.nguyen101@gmail.com>
2021-04-15 11:27:51 +0300

    create data module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import diaryRouter from './routes/diaries';
+
 const app = express();
 app.use(express.json());
 
@@ -8,6 +10,8 @@ app.get('/ping', (_req, res) => {
 	console.log('there was a ping...');
 	res.send('pong');
 });
+
+app.use('/api/diaries', diaryRouter);
 
 app.listen(PORT, () => {
 	console.log(`server running on port ${PORT}`);
