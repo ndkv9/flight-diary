fc7c7ed506fb4d545c8f35f68197972f8bd8ba57
ndkv9 <vu.nguyen101@gmail.com>
2021-04-15 21:51:25 +0300

    add get a single entry router

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -6,6 +6,15 @@ router.get('/', (_req, res) => {
 	res.send(diaryService.getNonSensitiveEntries());
 });
 
+router.get('/:id', (req, res) => {
+	const diary = diaryService.findById(Number(req.params.id));
+	if (!diary) {
+		res.status(404).end();
+	}
+
+	res.send(diary);
+});
+
 router.post('/', (_req, res) => {
 	res.send('Saving a diary!');
 });
